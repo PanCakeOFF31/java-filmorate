b222054bee7f0329a0f6e17c4eba3bc5e9bd6b80
DanSBolt <dan.s.boltachev@yandex.ru>
2024-04-05 23:23:08 +0300

    fix: удалить неиспользуемый импорт MethodNotImplemented в UserController (Спринт 12, групповой проект)

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.MethodNotImplemented;
 import ru.yandex.practicum.filmorate.model.*;
 import ru.yandex.practicum.filmorate.service.EventService;
 import ru.yandex.practicum.filmorate.service.UserService;
